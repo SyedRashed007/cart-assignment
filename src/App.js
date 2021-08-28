194005538f4b2388e80ea493910ad8b3823f29ff
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-08-28 14:50:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Sidebar from './components/Sidebar/Sidebar'
+import Navbar  from './components/Navbar/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
+
